51d8ab6927687892f98ba5e4585aca3bf4fc54e8
idstck <idstack@outlook.co.id>
2018-04-17 19:01:08 +0700

    change dashboard template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,65 @@
-@extends('layouts.app')
+@extends('layouts.admin.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">My Dashboard</li>
+    </ol>
+    <!-- Icon Cards-->
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
+        <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-success o-hidden h-100">
+                <div class="card-body">
+                    <div class="card-body-icon">
+                        <i class="fa fa-fw fa-list"></i>
+                    </div>
+                    <div class="mr-5">11 Posts!</div>
+                </div>
+                <a class="card-footer text-white clearfix small z-1" href="#">
+                    <span class="float-left">View Details</span>
+                    <span class="float-right">
+                        <i class="fa fa-angle-right"></i>
+                    </span>
+                </a>
+            </div>           
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-3">
+             <div class="card text-white bg-primary o-hidden h-100">
+                <div class="card-body">
+                    <div class="card-body-icon">
+                        <i class="fa fa-fw fa-comments"></i>
+                    </div>
+                    <div class="mr-5">26 New Comments!</div>
+                </div>
+                <a class="card-footer text-white clearfix small z-1" href="#">
+                    <span class="float-left">View Details</span>
+                    <span class="float-right">
+                        <i class="fa fa-angle-right"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                Dashboard
+                </div>
+                <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <h5 class="card-title">Welcome, Admin</h5>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    </div>
 @endsection
---
+++ resources/views/layouts/admin/partials/_navbar.blade.php
@@ -0,0 +1,80 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <a class="navbar-brand" href="#">{{ config('app.name', 'IDBlog') }}</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+            <a class="nav-link" href="#">
+            <i class="fa fa-fw fa-home"></i>
+            <span class="nav-link-text">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Posts">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
+            <i class="fa fa-fw fa-pencil"></i>
+            <span class="nav-link-text">Posts</span>
+            </a>
+            <ul class="sidenav-second-level collapse" id="collapseMulti">
+            <li>
+                <a href="#">All Posts</a>
+            </li>
+            <li>
+                <a href="#">Add New</a>
+            </li>
+            <li>
+                <a href="#">Categories</a>
+            </li>
+            </ul>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Comments">
+            <a class="nav-link" href="#">
+            <i class="fa fa-fw fa-comments"></i>
+            <span class="nav-link-text">Comments</span>
+            </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Users">
+            <a class="nav-link" href="#">
+            <i class="fa fa-fw fa-users"></i>
+            <span class="nav-link-text">Users</span>
+            </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Settings">
+            <a class="nav-link" href="#">
+            <i class="fa fa-fw fa-gears"></i>
+            <span class="nav-link-text">Settings</span>
+            </a>
+        </li>
+        </ul>
+        <ul class="navbar-nav sidenav-toggler">
+        <li class="nav-item">
+            <a class="nav-link text-center" id="sidenavToggler">
+            <i class="fa fa-fw fa-angle-left"></i>
+            </a>
+        </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a href="{{ url('/') }}" class="nav-link">
+            <i class="fa fa-fw fa-sign-in"></i>Site</a>
+        </li>
+        <li class="nav-item">
+            <form class="form-inline my-2 my-lg-0 mr-lg-2">
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Search for...">
+                <span class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+                </span>
+            </div>
+            </form>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+            <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+        </li>
+        </ul>
+    </div>
+</nav>
