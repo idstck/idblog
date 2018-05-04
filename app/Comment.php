92c6acf6103b0b36afb4390d84f90fe62b7f749e
idstck <idstack@outlook.co.id>
2018-05-04 18:29:56 +0700

    Home - Show Latest Comment in Widget

--- app/Comment.php
+++ app/Comment.php
@@ -9,4 +9,9 @@ class Comment extends Model
     protected $fillable = [
         'post_id', 'name', 'email', 'body', 'status'
     ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- resources/views/layouts/blog/partials/_widget-latest.blade.php
+++ resources/views/layouts/blog/partials/_widget-latest.blade.php
@@ -3,35 +3,22 @@
         <h3 class="h6">Latest Comments</h3>
     </header>
     <div class="blog-posts">
-        <a href="#">
-            <div class="item d-flex align-items-center">
-                <div class="image"><img src="{{ asset('assets/blog/img/small-thumbnail-1.jpg') }}" alt="..." class="img-fluid"></div>
-                <div class="title"><strong>Alberto Savoia Can Teach You About</strong>
-                <div class="d-flex align-items-center">
-                    <div class="comments"><i class="icon-comment"></i>12</div>
+        @foreach(\App\Comment::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get() as $comment)
+            <a href="{{ url('/blog/' . $comment->post->slug) }}">
+                <div class="item d-flex align-items-center">
+                    <div class="image">
+                        <img src="{{ asset('assets/blog/img/user.svg') }}" alt="" class="img-fluid">
+                    </div>
+                    <div class="title">
+                        <strong>{{ $comment->name }} On {{ $comment->post->title }}</strong>
+                    </div>
                 </div>
+                <div class="f-flex align-items-center">
+                    <p class="text-muted">
+                        {{ substr($comment->body, 0, 100) }}
+                    </p>
                 </div>
-            </div>
-        </a>
-        <a href="#">
-            <div class="item d-flex align-items-center">
-                <div class="image"><img src="{{ asset('assets/blog/img/small-thumbnail-2.jpg') }}" alt="..." class="img-fluid"></div>
-                <div class="title"><strong>Alberto Savoia Can Teach You About</strong>
-                <div class="d-flex align-items-center">
-                    <div class="comments"><i class="icon-comment"></i>12</div>
-                </div>
-                </div>
-            </div>
-        </a>
-        <a href="#">
-            <div class="item d-flex align-items-center">
-                <div class="image"><img src="{{ asset('assets/blog/img/small-thumbnail-3.jpg') }}" alt="..." class="img-fluid"></div>
-                <div class="title"><strong>Alberto Savoia Can Teach You About</strong>
-                <div class="d-flex align-items-center">
-                    <div class="comments"><i class="icon-comment"></i>12</div>
-                </div>
-                </div>
-            </div>
-        </a>
+            </a>
+        @endforeach
     </div>
 </div>    
