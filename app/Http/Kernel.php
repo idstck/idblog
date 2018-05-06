48851a68679ca7b0df6ec9c40804d8fdea231ee9
idstck <idstack@outlook.co.id>
2018-05-06 13:54:36 +0700

    Middleware - Make Role Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'role' => \App\Http\Middleware\Role::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
